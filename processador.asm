5bbd8650d5e3a1ef731164df386c2f3d04b843dc
Higor Ferreira <hfashigor@hotmail.com>
2018-12-14 16:40:46 -0200

    Ajusts

--- processador.asm
+++ processador.asm
@@ -65,7 +65,7 @@ Exec_add_8:
       pop rax      ;Restaurando o valor de rax
                    ;rdx é o registrador certo pra somar pois ele percorrerá o vetor de registradores
       Mov cl, byte [rdx]        ;Movendo o resultado do ponteiro
-      Mov ch, byte [rdx+bx*4]   ;
+      Mov ch, byte [rdx+bx*4]
 
       ;push rax
 
